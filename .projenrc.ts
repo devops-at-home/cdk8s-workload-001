d058e1f8d8d9d6c4accf70f915a11dccd16f3520
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2023-03-22 22:51:59 +0800

    chore: project created with projen

---
+++ .projenrc.ts
@@ -0,0 +1,15 @@
+import { cdk8s } from "projen";
+const project = new cdk8s.Cdk8sTypeScriptApp({
+  cdk8sVersion: "2.3.33",
+  defaultReleaseBranch: "main",
+  eslint: false,
+  name: "cdk8s-workload-001",
+  prettier: true,
+  projenrcTs: true,
+
+  // deps: [],                /* Runtime dependencies of this module. */
+  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+  // devDeps: [],             /* Build dependencies for this module. */
+  // packageName: undefined,  /* The "name" in package.json. */
+});
+project.synth();
