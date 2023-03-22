1736e2268fd58f793604bea2205523f9c168c7c1
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2023-03-22 22:54:33 +0800

    configure projen

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,15 +1,31 @@
-import { cdk8s } from "projen";
-const project = new cdk8s.Cdk8sTypeScriptApp({
-  cdk8sVersion: "2.3.33",
-  defaultReleaseBranch: "main",
-  eslint: false,
-  name: "cdk8s-workload-001",
-  prettier: true,
-  projenrcTs: true,
+import { cdk8s } from 'projen';
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+const project = new cdk8s.Cdk8sTypeScriptApp({
+    authorName: 'DevOps@Home',
+    authorUrl: 'https://devops-at-ho.me',
+    cdk8sVersion: '2.3.33',
+    defaultReleaseBranch: 'main',
+    eslint: false,
+    name: 'cdk8s-workload-001',
+    prettier: true,
+    prettierOptions: {
+        settings: {
+            printWidth: 100,
+            tabWidth: 4,
+            singleQuote: true,
+        },
+    },
+    githubOptions: {
+        pullRequestLint: false,
+    },
+    gitignore: ['.idea'],
+    projenrcTs: true,
+    licensed: false,
+
+    // deps: [],                /* Runtime dependencies of this module. */
+    // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
+    // devDeps: [],             /* Build dependencies for this module. */
+    // packageName: undefined,  /* The "name" in package.json. */
 });
+
 project.synth();
