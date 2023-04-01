02dd8c2348a27032b10a1a2fd6028195f92ab446
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2023-04-01 22:23:53 +0800

    remove aws-encryption-provider

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { App } from 'cdk8s';
-import { AwsEncryptionProvider } from './charts/aws-encryption-provider.chart';
 import { HelloKubernetes } from './charts/hello-kubernetes.chart';
 
 const app = new App();
 
 new HelloKubernetes(app, 'HelloKubernetes');
-
-new AwsEncryptionProvider(app, 'AwsEncryptionProvider', {
-    account: process.env.ACCOUNT_ID,
-    key: process.env.KEY_ID,
-});
 
 app.synth();
