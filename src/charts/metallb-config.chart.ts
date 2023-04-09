9591e800b1cf6e0140353217ad6222026c3409af
devops-at-home <76842834+devops-at-home@users.noreply.github.com>
2023-04-09 21:27:49 +0800

    update metallb chart and tests

--- src/charts/metallb-config.chart.ts
+++ src/charts/metallb-config.chart.ts
@@ -2,15 +2,17 @@ import { Chart, ChartProps } from 'cdk8s';
 import { Construct } from 'constructs';
 import { IpAddressPool, L2Advertisement } from '../imports/metallb.io';
 
-interface MetalLbConfigProps extends ChartProps {
+export type MetalLbConfigParams = {
     addresses: string[];
-}
+};
+
+interface MetalLbConfigProps extends ChartProps, Partial<MetalLbConfigParams> {}
 
 export class MetalLbConfig extends Chart {
     constructor(scope: Construct, id: string, props: MetalLbConfigProps) {
         super(scope, id, props);
 
-        const { addresses } = props;
+        const { addresses } = parseInputs(props);
 
         const ipAddressPool = new IpAddressPool(this, 'IpAddressPool', {
             spec: {
@@ -26,3 +28,9 @@ export class MetalLbConfig extends Chart {
         });
     }
 }
+
+export const parseInputs = (params: Partial<MetalLbConfigParams>): MetalLbConfigParams => {
+    return {
+        addresses: params.addresses ?? ['127.0.0.1/8'],
+    };
+};
--- test/charts/metallb-config.test.ts
+++ test/charts/metallb-config.test.ts
@@ -1,5 +1,9 @@
 import { App } from 'cdk8s';
-import { MetalLbConfig } from '../../src/charts/metallb-config.chart';
+import {
+    MetalLbConfig,
+    MetalLbConfigParams,
+    parseInputs,
+} from '../../src/charts/metallb-config.chart';
 
 const chart = 'metallb-config';
 
@@ -9,9 +13,24 @@ describe(chart, () => {
 
         const manifest = new MetalLbConfig(app, chart, {
             namespace: 'kube-system',
-            addresses: ['127.0.0.1/8'],
         });
 
         expect(manifest.toJson()).toMatchSnapshot();
+    });
+});
+
+describe('Unit tests', () => {
+    test('parseInputs function default values', () => {
+        const response = parseInputs({});
+        expect(response.addresses).toStrictEqual(['127.0.0.1/8']);
+    });
+
+    test('parseInputs function with inputs', () => {
+        const inputs: MetalLbConfigParams = {
+            addresses: ['192.168.1.1/32'],
+        };
+
+        const response = parseInputs(inputs);
+        expect(response.addresses).toStrictEqual(inputs.addresses);
     });
 });
